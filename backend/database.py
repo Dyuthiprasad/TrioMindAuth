4a1b3a84e1dcaeeaf8ba0ac4766088fe77d4ba3a
Dyuthiprasad <dyuthi.cs23@bmsce.ac.in>
2025-07-30 16:15:43 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -16,3 +16,6 @@ db = client["triomind_db"]
 
 # Access collection
 users_collection = db["users"]
+results_collection = db["results"]
+ai_feedback_collection = db["ai_feedback"]
+user_feedback_collection = db["user_feedback"]
